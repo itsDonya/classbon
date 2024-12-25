80612d905e4e4a6ef3689621a48935a8c245dda1
Donya <thisis2nya@gmail.com>
2024-12-26 01:18:39 +0330

    feat: handle button's different props and types using classNames npm package

--- src/app/_components/button/button.tsx
+++ src/app/_components/button/button.tsx
@@ -1,4 +1,21 @@
-import { ButtonProps } from "./button.types";
+import { Size } from "../types/size.type";
+import { ButtonProps, ButtonShape } from "./button.types";
+
+import classNames from "classnames";
+
+const sizeClasses: Record<Size, string> = {
+  tiny: "btn-xs",
+  small: "btn-sm",
+  normal: "",
+  large: "btn-lg",
+};
+
+const shapeClasses: Record<ButtonShape, string> = {
+  wide: "btn-wide",
+  full: "btn-block",
+  square: "btn-square",
+  default: "",
+};
 
 export const Button: React.FC<ButtonProps> = ({
   variant,
@@ -16,8 +33,20 @@ export const Button: React.FC<ButtonProps> = ({
   className,
   ...rest
 }: ButtonProps) => {
+  const classes = classNames(
+    "btn",
+    className,
+    { "btn-outlined": isOutline },
+    { "btn-link": isLink },
+    { "animated-icon": animatedIcon },
+    { "pointer-events-none opacity-80": isLoading },
+    { [`btn-${variant}`]: variant },
+    { [`${sizeClasses[size]}`]: size },
+    { [`${shapeClasses[shape]}`]: shape }
+  );
+
   return (
-    <button type={type} disabled={isDisabled} {...rest}>
+    <button type={type} disabled={isDisabled} {...rest} className={classes}>
       {isLoading ? loadingText : children}
     </button>
   );
