a11051824fc85950e0d1e69a1ff315594ab8dcf0
Donya <thisis2nya@gmail.com>
2024-12-26 00:31:55 +0330

    feat: create landing hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
-import { Colors } from "./_components/colors/colors";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="size-full my-auto flexs items-center justify-center">
-      <p className="text-xl text-neutral-600">صفحه اصلی</p>
-      {/* <Colors /> */}
-    </div>
+    <>
+      <section className="bg-hero-pattern bg-no-repeat bg-center mt-5 xl:mt-20 xl:bg-left">
+        <div className="container flex flex-col-reverse items-center xl:flex-row">
+          {/* right */}
+          <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
+            <h3 className="text-xl dark:text-info xl:text-2xl">
+              خوش اومدی به ...
+            </h3>
+            <h1 className="text-3xl font-black gradient lg:text-5xl">
+              مسیر صعود به قله‌های برنامه‌نویسی
+            </h1>
+            <p>
+              هر جای مسیر برنامه‌نویسی که باشی، با همراهی استادهای باتجربه‌ی
+              کلاسبن میتونی بدون محدودیت به قله‌های بالاتر، صعود کنی. ما همیشه
+              هواتو داریم
+            </p>
+
+            <Image
+              src="/images/frameworks.png"
+              width={412}
+              height={39}
+              className="grayscale mt-4 opacity-70 m-auto xl:m-0"
+              alt=""
+            />
+          </div>
+
+          {/* left */}
+          <Image
+            src="/images/programmer-landing.svg"
+            width={702}
+            height={521}
+            alt="کلاسبن"
+          />
+        </div>
+      </section>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,6 +54,11 @@ const config: Config = {
     container: {
       center: true,
     },
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url(/images/tile.svg)",
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
