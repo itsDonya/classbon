3a28d423088430de2f7c078b14c8a1b11a0c1c63
Donya <thisis2nya@gmail.com>
2024-12-25 20:39:22 +0330

    core: add Ravi local font to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,51 @@
 import "./globals.css";
 
 // fonts
+import localFont from "next/font/local";
 import { Figtree } from "next/font/google";
 
+// figtree font config
 const figtree = Figtree({
   display: "swap",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-figtree",
+});
+
+const ravi = localFont({
+  src: [
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ravi/RaviFaNum-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-ravi",
 });
 
 export default function RootLayout({
@@ -16,7 +54,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl" className={`${figtree.variable}`}>
+    <html dir="rtl" className={`${ravi.variable} ${figtree.variable}`}>
       <body className="antialiased flex flex-col h-screen">
         <header className="w-full h-16 bg-neutral-200 flex items-center justify-center">
           <p className="text-xl text-neutral-700 font-bold uppercase">header</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Colors } from "./_components/colors/colors";
 export default function Home() {
   return (
     <div className="size-full my-auto flexs items-center justify-center">
-      {/* <p className="text-xl text-neutral-600">This is home page</p> */}
-      <Colors />
+      <p className="text-xl text-neutral-600">صفحه اصلی</p>
+      {/* <Colors /> */}
     </div>
   );
 }
